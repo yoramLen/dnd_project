38c9e41548ab91fd2c47ed9c61cd0861d0c52e00
yoramLen <yoram.lenaers@student.ap.be>
2021-12-22 12:58:01 +0100

    contact form implemented, refresh after 2 secs to empty form without using state cuz lazy

--- src/components/Contactform/Contactform.tsx
+++ src/components/Contactform/Contactform.tsx
@@ -1,48 +1,88 @@
 import {useState, useEffect} from 'react';
-import { Alert } from 'react-bootstrap';
+import { Alert, Button, Col, Form, Row } from 'react-bootstrap';
+import { BrowserRouter } from 'react-router-dom';
+import styles from "./Contactform.module.css";
 
+
+interface FormInput{
+  firstName: string,
+  lastName: string,
+  email: string,
+  message:string,
+  address:string
+}
 
 const Contactform = () =>{
     const [sent, setSent] = useState<boolean>(false);
+
 
 
     useEffect(()=>{
         setSent(false);
     }, [])
 
+
+    const SubmitHandler = ()=>{
+      setSent(true);
+
+      setTimeout(()=>{
+        window.location.reload()
+      },2000)
+
+    }
+
+
 return(
-    <div>
-        {sent?<Alert variant='success'>Your email was sent</Alert>: <div></div>}
-    <form className="form">
+  
+    <div className={styles.formAlign}>
+      {sent?<Alert variant='success' className={styles.formWidth}>Your Email was sent successfully</Alert>:""}
+            <Form className={styles.formWidth}>
+  <Row className="mb-3">
+    <Form.Group as={Col} controlId="formGridEmail">
+      <Form.Label className={styles.formlabel}>Email</Form.Label>
+      <Form.Control type="email" placeholder="Enter email" />
+    </Form.Group>
 
-<div className="name">
+  </Row>
+  <Row className='mb-3'>
+  <Form.Group as={Col} controlId="formGridPassword">
+      <Form.Label>First Name</Form.Label>
+      <Form.Control type="first name" placeholder="John" />
+    </Form.Group>
+    <Form.Group as={Col} controlId="formGridPassword">
+      <Form.Label>First Name</Form.Label>
+      <Form.Control type="last name" placeholder="Doe"  />
+    </Form.Group>
 
-   <label htmlFor="firstName">First name</label>
-   <input
-        type="text"
-        name="firstName"
-        className="firstName"
-   />
-   <label htmlFor="lastName">Last name</label>
-   <input
-        type="text"
-        name="lastName"
-        className="forminput"
-        
-   />
-</div>
-<label htmlFor="message">Message</label>
 
-<textarea
-  placeholder="Start typing..."
-  className="message"
-  name="message"
-/>
-</form>
-<button type="submit" className="send" onClick={()=>setSent(true)}>Send</button>
+      
+  </Row>
+<Row>
+<Form.Group className="mb-3" controlId="formGridAddress1">
+    <Form.Label>Address</Form.Label>
+    <Form.Control placeholder="1234 Main St" />
+  </Form.Group>
+</Row>
+
+  <Row className="mb-3">
+  <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+    <Form.Label>Example textarea</Form.Label>
+    <Form.Control as="textarea" rows={3} />
+  </Form.Group>
+  </Row>
+
+
+  <Button variant="danger" type="submit" onClick={()=>{SubmitHandler()}}>
+    Submit
+  </Button>
+</Form>
 
 
     </div>
+
+
+
+
 )
 
 
--- src/components/Spells/ContactPage/ContactPage.tsx
+++ src/components/Spells/ContactPage/ContactPage.tsx
@@ -6,7 +6,7 @@ const ContactPage = () => {
         <div className='contentWrapper'>
             <h1 className={styles.centertext}>Contact</h1>
         <p className={styles.centertext}>Please do not contact us and if you feel like contacting us then use the form below. We promise to eventually reply (maybe).</p>
-       <div className={styles.centertext}>
+       <div className={`${styles.formWidth} ${styles.formlabel} ${styles.formAlign}`}>
        <Contactform/>
 
        </div>
